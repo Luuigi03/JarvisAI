863b54cc9bc23bd586dfd92e843f53add5644bc1
LuigiCocco <l.cocco1@studenti.unisa.it>
2026-02-13 17:03:34 +0100

    aggiunta ui statica

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import os #interazione con sistema operativo
 import google.generativeai as genai #sdk per utilizzare gemini
 import threading # concorrenza
 from dotenv import load_dotenv #gestione variabili d'ambiente
-import weather #api meteo
+import weather #api meteo 
+import pygame as pg# libreria per ui
+import threading #La ui sarà gestita su un thread separato da quello della logica di ascolto ecc..
 
 # Carica variabili d'ambiente che sono scritte nel file .env
 load_dotenv()
@@ -24,7 +26,7 @@ engine = pyttsx3.init() # oggetto per la gestione dello TTS
 engine.setProperty('rate', 150)  # Velocità voce. Rate=parole al minuto
 engine.setProperty('volume', 1.0) # 1.0 = volume al massimo
 
-# Variabili globali
+#Variabili globali
 stop_speaking = False
 NOME_PREFERITO = "Iron Man"
 
@@ -43,18 +45,16 @@ def parla(testo):
     global stop_speaking
     stop_speaking = False
 
-    def _speak():
-        if stop_speaking: return
-        try:
-            engine.say(testo)
-            engine.runAndWait() #multithreading
-        except RuntimeError:
-            # Gestisce il caso in cui il loop dell'engine sia già attivo
-            pass
+    # Debug visivo per essere sicuri che la funzione venga chiamata
+    print(f"🔊 Tentativo di riproduzione audio: {testo}")
 
-    # Thread per non bloccare l'ascolto mentre parla, multithreading
-    thread = threading.Thread(target=_speak, daemon=True)
-    thread.start()
+    try:
+        engine.say(testo)
+        engine.runAndWait() # Questo blocca il thread 'logica_jarvis' finché non finisce di parlare
+    except RuntimeError:
+        # Gestisce il caso in cui il loop dell'engine sia già attivo (raro se non usi thread annidati)
+        print("Errore: Engine loop già attivo")
+        pass
 
 
 def ascolta():
@@ -86,6 +86,7 @@ def ascolta():
 #pone la domanda a gemini e ritorna la risposta
 def rispondi_gemini(domanda):
 
+
     try:
         response = chat.send_message(domanda)
         return response.text
@@ -93,52 +94,89 @@ def rispondi_gemini(domanda):
         return f"Mi dispiace, c'è stato un errore nei miei circuiti: {str(e)}"
 
 
-# --- LOOP PRINCIPALE ---
-print("\n--- JARVIS ATTIVO ---")
-parla(f"Sistemi online. Ciao {NOME_PREFERITO}.")
 
-while True:
-    testo_utente = ascolta()
+def main_ui():
+    pg.init()
+    screen = pg.display.set_mode((500, 500))
+    clock = pg.time.Clock()
+    running = True
 
-    if not testo_utente:
-        continue  # Se non sente nulla, ricomincia subito il ciclo (reattività)
+    while running:
+        for event in pg.event.get(): #recupera tutti gli eventi in coda(movimenti del mouse, interrupt tastiera ecc...)
+            if event.type == pg.QUIT: running = False # Controlla se l'utente ha chiuso la finestra pigiando la x
 
-    # 1. Comandi di Uscita
-    if any(parola in testo_utente for parola in ["esci", "spegniti", "addio", "stop"]):
-        parla("Disattivazione sistemi. A presto.")
-        engine.stop()
-        
+        screen.fill((10, 10, 25)) #Sfondo scuro
+        color = (0, 200, 255)
+        pg.draw.circle(screen, color, (250, 250), 100, 2)
+        pg.display.flip() #mostra ciò che è stato disegnato
+        clock.tick(60) #60fps
 
-    # 2. Rilevamento Parola Chiave "Jarvis" (o varianti)
-    keyword_detected = any(k in testo_utente for k in ["jarvis", "giarvis", "ciarvis"])
+#ui_thread = threading.Thread(target = avvia_interfaccia, daemon = True)
+#ui_thread.start()
 
-    if keyword_detected:
-        # Pulisce la frase togliendo la parola chiave per mandarla pulita alle funzioni
-        comando_pulito = testo_utente.replace("jarvis", "").replace("giarvis", "").strip()
+    
 
-        # Se ha detto SOLO "Jarvis", chiedi cosa vuole
-        if not comando_pulito:
-            parla("Dimmi pure?")
-            continue
+def logica_jarvis():
+    # --- LOOP PRINCIPALE ---
+    print("\n--- JARVIS ATTIVO ---")
+    parla(f"Sistemi online. Ciao {NOME_PREFERITO}.")
 
-        # 3. Controllo Meteo
-        keywords_meteo = ["meteo", "tempo", "previsioni", "piove", "gradi"]
-        if any(w in comando_pulito for w in keywords_meteo):
-            citta = weather.trova_citta(comando_pulito)
-            if citta:
-                print(f"🔍 Controllo meteo per: {citta}")
-                dati_meteo = weather.previsione_meteo(citta)
-                parla(dati_meteo)
-            else:
-                # Se non trova la città, lo chiede a Gemini
-                risposta = rispondi_gemini(comando_pulito)
-                parla(risposta)
-            continue
+    while True:
+        testo_utente = ascolta()
+        if not testo_utente:
+            continue  # Se non sente nulla, ricomincia subito il ciclo (reattività)
 
-        # 4. Risposta Generica (Gemini)
-        print("🤖 Elaborazione risposta...")
-        risposta = rispondi_gemini(comando_pulito)
+        # 1. Comandi di Uscita
+        if any(parola in testo_utente for parola in ["esci", "spegniti", "addio", "stop"]):
+            parla("Disattivazione sistemi. A presto.")
+            stop_speaking = True
+            engine.endLoop(True)
 
-        # Stampa e Parla
-        print(f"Jarvis: {risposta}")
-        parla(risposta)
+            
+
+        # 2. Rilevamento Parola Chiave "Jarvis" (o varianti)
+        keyword_detected = any(k in testo_utente for k in ["jarvis", "giarvis", "ciarvis"])
+
+        if keyword_detected:
+            # Pulisce la frase togliendo la parola chiave per mandarla pulita alle funzioni
+            comando_pulito = testo_utente.replace("jarvis", "").replace("giarvis", "").strip()
+
+            # Se ha detto SOLO "Jarvis", chiedi cosa vuole
+            if not comando_pulito:
+                parla("Dimmi pure?")
+                continue
+
+            # 3. Controllo Meteo
+            keywords_meteo = ["meteo", "tempo", "previsioni", "piove", "gradi"]
+            if any(w in comando_pulito for w in keywords_meteo):
+                citta = weather.trova_citta(comando_pulito)
+                if citta:
+                    print(f"🔍 Controllo meteo per: {citta}")
+                    dati_meteo = weather.previsione_meteo(citta)
+                    parla(dati_meteo)
+                else:
+                    # Se non trova la città, lo chiede a Gemini
+                    risposta = rispondi_gemini(comando_pulito)
+                    parla(risposta)
+                continue
+
+            # 4. Risposta Generica (Gemini)
+            print("🤖 Elaborazione risposta...")
+            risposta = rispondi_gemini(comando_pulito)
+
+            # Stampa e Parla
+            print(f"Jarvis: {risposta}")
+            parla(risposta)
+
+
+if __name__ == "__main__":
+    #Inizialmente avvio la logica di jarvis su un thread secondario
+    #macos richiede che la ui venga eseguita sul thread principale
+    thread_jarvis = threading.Thread(target = logica_jarvis, daemon = True)
+    thread_jarvis.start()
+
+    print("Avvio UI...")
+    main_ui()
+    
+    
+    
